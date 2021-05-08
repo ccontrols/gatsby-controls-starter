e8742068fed9ab847e3ef1e78766eeda47cf5e86
atanasster <atanasster@gmail.com>
2021-05-08 23:46:28 +0300

    data driven tests

---
+++ src/components/VariantButton/VariantButton.data.ts
@@ -0,0 +1,24 @@
+export default {
+  overview: {
+    "0": {
+      text: "Leone Kirlin",
+      icon: "Dariana Osinski"
+    },
+    "1": {
+      text: "Winston Schumm",
+      icon: "Marilie Jacobson DVM"
+    },
+    "2": {
+      text: "Concepcion Mayer",
+      icon: "Lexi Trantow"
+    },
+    "3": {
+      text: "Darion Adams",
+      icon: "Nola Marks"
+    },
+    "4": {
+      text: "Adalberto Bartell",
+      icon: "Jennyfer Corkery I"
+    }
+  }
+};
