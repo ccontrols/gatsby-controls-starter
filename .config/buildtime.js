27f1f76ef922a98e2b7a4e98066b7a073bc7b7e9
atanasster <atanasster@gmail.com>
2021-05-13 12:37:32 +0300

    clean config

--- .config/buildtime.js
+++ .config/buildtime.js
@@ -1,7 +1,4 @@
 module.exports = {
   stories: ["../src/docs/*.mdx", "../src/components/**/*.docs.tsx"],
-  pages: {
-    story: {},
-  },
   siteUrl: `https://gatsby-controls-starter.netlify.app`,
 };
